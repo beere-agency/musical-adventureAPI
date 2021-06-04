c795c2f1a74e47678e283225a95b9b885b3e424b
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-06-04 20:05:23 +0100

    Category Service

---
+++ MAService/Contract/CategoryRepository.cs
@@ -0,0 +1,59 @@
+﻿using MAData;
+using MADomain;
+using MAService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAService.Contract
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext ctx;
+        public CategoryRepository(ApplicationDbContext _ctx)
+        {
+            ctx = _ctx;
+        }
+
+        public void Create(Category category)
+        {
+            if (category == null)
+                throw new ArgumentNullException(nameof(category));
+
+            ctx.Categories.Add(category);
+            SaveChanges();
+        }
+
+        public Category GetById(int Id)
+        {
+            if (Id <= 0)
+                throw new ArgumentNullException("Category");
+            return ctx.Categories.FirstOrDefault(c => c.Id == Id);
+        }
+
+        public IQueryable<Category> GetClients()
+        {
+            return ctx.Categories;
+        }
+
+        public void Update(Category category)
+        {
+            ctx.Categories.Update(category);
+            SaveChanges();
+        }
+
+        public void Delete(Category category)
+        {
+            ctx.Categories.Remove(category);
+
+            SaveChanges();
+        }
+
+        public bool SaveChanges()
+        {
+            return (ctx.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ MAService/Interface/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using MADomain;
+using System.Linq;
+
+namespace MAService.Interface
+{
+    public interface ICategoryRepository
+    {
+        void Create(Category category);
+        void Delete(Category category);
+        Category GetById(int Id);
+        IQueryable<Category> GetClients();
+        void Update(Category category);
+    }
+}
