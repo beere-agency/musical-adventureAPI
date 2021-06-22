6d2d0df78d5c77f209afba4ca5847fa52a18cf4d
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-06-22 20:16:27 +0100

    Update on Product mapping and

---
+++ MAData/Migrations/20210622184841_Many to Many Mapping update.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MAData.Migrations
+{
+    public partial class ManytoManyMappingupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Products_ProductId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Products_ProductId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_ProductId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_ProductId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Categories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Tags",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_ProductId",
+                table: "Tags",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ProductId",
+                table: "Categories",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Products_ProductId",
+                table: "Categories",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Products_ProductId",
+                table: "Tags",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- MADomain/Product.cs
+++ MADomain/Product.cs
@@ -7,7 +7,7 @@ namespace MADomain
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
-        public List<Category> Categories { get; set; }
-        public List<Tag> Tags { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+        public List<ProductTag> ProductTags { get; set; }
     }
 }
