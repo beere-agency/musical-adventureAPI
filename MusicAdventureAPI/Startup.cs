fe7eee536b0a9b9e51383d499ca3643f17a84c41
Darkace01 <quadrikazeem01@gmail.com>
2021-07-05 15:49:22 +0100

    Update on Swagger Configuration

--- MusicAdventureAPI/Startup.cs
+++ MusicAdventureAPI/Startup.cs
@@ -49,35 +49,8 @@ namespace MusicAdventureAPI
             // Adding Authentication  
             services.ConfigureJWT(Configuration);
 
-            services.AddSwaggerGen(swagger =>
-            {
-                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "MusicAdventureAPI", Version = "v1" });
-
-                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-                });
-                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                          new OpenApiSecurityScheme
-                            {
-                                Reference = new OpenApiReference
-                                {
-                                    Type = ReferenceType.SecurityScheme,
-                                    Id = "Bearer"
-                                }
-                            },
-                            new string[] {}
-
-                    }
-                });
-            });
+            //Swagger Configuration
+            services.ConfigureSwagger();
 
             //Dependency Injection
             services.ConfigureRepository();
